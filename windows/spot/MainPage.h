cf538db18229c227be7ffd9ba90e86e5218ea133
adex-codez <adeaduratimilehin@gmail.com>
2024-06-28 14:29:07 +0100

    first commit

---
+++ windows/spot/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::spot::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::spot::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
